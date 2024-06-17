f44815cef20c3bf32e807f1584f538f74caac867
Matheus Mendes <matheus_mendess@live.com>
2024-06-17 16:03:51 -0300

    feat: jwt strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
+import { envSchema } from './env';
+import { authModule } from './auth/auth.module';
+import { AuthenticationController } from './controllers/authenticate.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    authModule,
+  ],
+  controllers: [
+    AppController,
+    CreateAccountController,
+    AuthenticationController,
+  ],
   providers: [AppService, PrismaService],
 })
 export class AppModule {}
